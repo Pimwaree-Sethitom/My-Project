ea96adbb2558d036b2e3aa7fbab2ef8a6ef7d5c2
Pimwaree-Sethitom <pimwaree9072@gmail.com>
2025-05-18 01:09:51 +0700

    ...

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: '', redirectTo: 'link_home', pathMatch: 'full' },
   { path: 'login', loadComponent: () => import('./login/login.component').then(m => m.LoginComponent) },
   { path: 'admin', loadComponent: () => import('./admin/admin.component').then(m => m.AdminComponent) },
   { path: 'research', loadComponent: () => import('./research/research.component').then(m => m.ResearchComponent) },
